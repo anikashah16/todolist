ddbddb361ac0ce2f7bd1381e204f6936d8346234
Anika <anikahs0516@gmail.com>
2021-07-20 10:42:20 -0400

    Add label

--- todolist/ViewController.swift
+++ todolist/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
-}
+var firstName = "Anika"
+    var age = 15
+    }
 
